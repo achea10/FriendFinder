73275a0625611dbd3f0d5c04f5cb5f20a4e6c45d
Achea Compton <achea_10@hotmail.com>
2017-11-02 14:50:07 -0700

    finished api codes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,18 @@
-var friendslist = 
+var friendsList = [
+	{
+		name: "Achea"
+		photo: "http://www.sillyjokes.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/m/image_14389.jpg"
+		answers: ["5", "3", "4", "2", "2", "1", "4", "3", "3", "1" ]
+	}, {	
+		name: "Courage the Cowardly Dog"
+		photo: "https://images4.alphacoders.com/226/226058.jpg"
+		answers: ["4", "3", "3", "1", "2", "4", "5", "1", "3", "4" ]
+
+	}, {
+		name: "Daria"
+		photo:"https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/TVSeries/Daria_580400-MTV_DARIA._RI_SX940_.jpg"
+		answers: ["2", "5", "3", "4", "4", "1", "2", "2", "4", "5" ]
+	}
+];
+
+module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,44 @@
-var 
+var friendsMatch = require('../data/friends.js');
+
+module.exports = function (app) {
+
+	app.get('api/friends', function(req, res) {
+		var newFriend = req.body;
+
+		for (var i = 0; i < newFriend.answers.length; i++) {
+			if (newFriend.answers [i] == "1 (Strongly Disagree)") {
+				newFriend.answers[i] = 1;
+			} else if (newFriend.answers[i] == "5 (Strongly Agree)") {
+				newFriend.answers[i] = 5;
+			} else {
+				newFriend.answers [i] == parseInt (newFriend.answers[i])
+			}
+		}
+
+		var differencesArray = [];
+
+		for(var i = 0; i < friendsMatch.length; i++) {
+			var compare = friendsMatch[i];
+			var totalDifference = 0;
+
+			for (var j = 0; j < compare.answers.length; j++) {
+				var differenceScoreOne = Math.abs(compare.answers[j] - newFriend.answers[j]);
+				totalDifference += differenceScoreOne;
+			}
+
+			differencesArray[i] = totalDifference;
+		}
+
+		var bestMatchNum = differencesArray[0];
+		var bestMatchIndex = 0
+
+		for(var i = 1; i <differencesArray.length; i++) {
+			if (differencesArray[i] < bestMatchNum) {
+				bestMatchNum = differencesArray[i];
+				bestMatchIndex = i;
+			}
+		}
+		friendsMatch.push(newFriend);
+		res.json(friendsMatch[bestMatchIndex]);
+	})
+}
